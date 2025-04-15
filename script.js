92c7f29f46fe9699a726137b3edee68ea260f899
ashish <ashishmasih92g@gmail.com>
2025-04-15 23:49:08 +0530

    assignment

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your JS code here. If required.
+let codes = document.querySelectorAll(".code")
+
+codes.forEach((val, index) => {
+
+    val.addEventListener("input", (e) => {
+        let value1 = e.target.value
+        if (value1) {
+            if (index < codes.length - 1) {
+                codes[index + 1].focus()
+            }
+        }
+
+    })
+
+    val.addEventListener("keydown", (e) => {
+        let keypress = e.key
+        let value2 = e.target.value
+        if (keypress == "Backspace") {
+
+
+            if(value2==""){
+                
+                codes[index-1].focus()
+                codes[index-1].value=""
+            }
+            else{
+                e.target.value=""
+            }
+        }
+
+    })
+
+})
+
+window.addEventListener('load', () => {
+    inputs[0].focus();
+  });
+
+
